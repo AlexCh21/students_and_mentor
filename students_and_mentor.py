51a3908ed2c2aeb197ecd2d3a69626a0e829a919
AlexCh21 <alexandr9@list.ru>
2021-10-12 16:20:34 +1100

    second commit

--- students_and_mentor.py
+++ students_and_mentor.py
@@ -42,16 +42,39 @@ class Lecturer(Mentor):
         return (f'Лектор \nИмя: {self.name} \nФамилия: {self.surname}')
                 #f'\nСредняя оценка за лекции: {self.average_rate()}\n')
 
+class Reviewer(Mentor):
+    def rate_hw(self, student, course, grade):
+        if isinstance(student, Student) and course in self.courses_attached and course in student.courses_in_progress:
+            if course in student.grades:
+                student.grades[course] += [grade]
+            else:
+                student.grades[course] = [grade]
+        else:
+            return 'Ошибка'
+
+    def __str__(self):
+        return f'Проверяющий \nИмя: {self.name} \nФамилия: {self.surname}\n'
+
 best_student_1 = Student('Иванов', 'Иван', 'boy')
 best_student_1.courses_in_progress += ['Python', 'Git']
+
 best_student_2 = Student('Иванова', 'Ольга', 'girl')
 best_student_2.courses_in_progress += ['Python']
 best_student_2.finished_courses += ['Git']
+
+reviewer_cool_mentor_2 = Reviewer('Анна', 'Сидорова')
+reviewer_cool_mentor_2.courses_attached += ['Python']
+reviewer_cool_mentor_1 = Reviewer('Антон', 'Сидоров')
+reviewer_cool_mentor_1.courses_attached += ['Git']
 
 lecturer_cool_mentor_3 = Lecturer('Марина', 'Петрова')
 lecturer_cool_mentor_3.courses_attached += ['Python']
 lecturer_cool_mentor_1 = Lecturer('Антон', 'Сидоров')
 lecturer_cool_mentor_1.courses_attached += ['Git']
+
+reviewer_cool_mentor_2.rate_hw(best_student_1, 'Python', 7)
+reviewer_cool_mentor_1.rate_hw(best_student_1, 'Git', 6)
+reviewer_cool_mentor_2.rate_hw(best_student_2, 'Python', 9)
 
 best_student_1.rate_lecturer(lecturer_cool_mentor_3, 'Python', 8)
 best_student_1.rate_lecturer(lecturer_cool_mentor_1, 'Git', 6)
@@ -59,6 +82,8 @@ best_student_2.rate_lecturer(lecturer_cool_mentor_3, 'Python', 9)
 
 print(best_student_1)
 print(best_student_2)
+print(reviewer_cool_mentor_2)
+print(reviewer_cool_mentor_1)
 print(lecturer_cool_mentor_3)
 print(lecturer_cool_mentor_1)
 
